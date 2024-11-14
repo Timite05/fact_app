13004c5e1d520eff48c9e5fa44697bbea0c55392
Timite05 <timite1145@gmail.com>
2024-11-14 17:11:30 +0100

    premier commit

--- django_invoice/settings.py
+++ django_invoice/settings.py
@@ -19,11 +19,18 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
+try:
+    from django.contrib.messages import constants as messages
+    MESSAGE_TAGS = {
+        messages.DEBUG: 'alert-info',
+        messages.INFO: 'alert-info',
+        messages.SUCCESS: 'alert-succes',
+        messages.WARNING: 'alert-warning',
+        messages.ERROR : 'alert-danger',
+    }
+except Exception as e:
+    pass
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-h@jho)@2siqzqo3qh3%f1l&#v3jtp@))ei3*611-7lg0d9e-z%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
--- fact_app/fact_app/views.py
+++ fact_app/fact_app/views.py
@@ -1,4 +1,19 @@
-from django.shortcuts import render  # type: ignore
+from django.shortcuts import render
+from django.views import View 
+from . models import * # type: ignore
 
-def home(request, *args, **kwargs):
-    return render(request, 'base.html')
+class HomeView(View):
+    templates_name = 'index.html'
+    invoices = Invoice.objects.select_related('customer','save_by').all()
+
+    context = {
+        'invoice': invoices
+    }
+    
+    def get(self, request, *args, **kwargs):
+
+        return render(request, self.templates_name, self.context)
+    
+    def post(self, request, *args, **kwargs):
+
+        return render(request, self.templates_name, self.context)
